19034d4b7c9a4bdb2038a3e0c3b31dee47a779e2
Zeinab Nasihatkon <zeinab@nematifamilyfundation.onmicrosoft.com>
2025-05-19 19:09:27 +0330

    Add cli-server

--- core/developer/codedeveloperlist.cpp
+++ core/developer/codedeveloperlist.cpp
@@ -17,7 +17,7 @@ CodeDeveloperList* CodeDeveloperList::instance(QObject* parent) {
 }
 
 CodeDeveloperList::CodeDeveloperList(QObject *parent)
-    : QAbstractListModel(parent)
+    : QAbstractListModel(parent), m_port(8080), m_isRuning(false)
 {
     QList<QPair<int, QString>> languages = {
         {0, "Curl"},
@@ -33,6 +33,22 @@ CodeDeveloperList::CodeDeveloperList(QObject *parent)
     m_currentProgramLanguage->setCodeGenerator(new CurlCodeGenerator());
 }
 
+bool CodeDeveloperList::isRuning() const{return m_isRuning;}
+void CodeDeveloperList::setIsRuning(bool newIsRuning){
+    if (m_isRuning == newIsRuning)
+        return;
+    m_isRuning = newIsRuning;
+    emit isRuningChanged();
+}
+
+int CodeDeveloperList::port() const{return m_port;}
+void CodeDeveloperList::setPort(int newPort){
+    if (m_port == newPort)
+        return;
+    m_port = newPort;
+    emit portChanged();
+}
+
 void CodeDeveloperList::setCurrentLanguage(int newId){
     for(int number=0; number< m_programLanguags.size(); number++){
         ProgramLanguage* programLanguage = m_programLanguags[number];
@@ -40,6 +56,99 @@ void CodeDeveloperList::setCurrentLanguage(int newId){
             setCurrentProgramLanguage(programLanguage);
         }
     }
+}
+
+void CodeDeveloperList::start() {
+    if (m_isRuning)
+        return;
+
+    m_httpServer = new QHttpServer(this);
+
+    auto createResponse = [](const QJsonObject &jsonObj) {
+        QByteArray body = QJsonDocument(jsonObj).toJson();
+        return QHttpServerResponse(body, "Hi dear How are you? ", QHttpServerResponse::StatusCode::Ok);
+    };
+
+
+    m_httpServer->route("/models/", QHttpServerRequest::Method::Get, [this, createResponse]() {
+        QJsonArray modelsArray;
+        for (const auto& lang : m_programLanguags) {
+            QJsonObject obj;
+            obj["id"] = lang->id();
+            obj["name"] = lang->name();
+            modelsArray.append(obj);
+        }
+        return createResponse(QJsonObject{{"models", modelsArray}});
+    });
+
+    m_httpServer->route("/models/", QHttpServerRequest::Method::Post, [createResponse]() {
+        return createResponse(QJsonObject{
+            {"error", QJsonObject{
+                          {"message", "POST not allowed on /models/. Use GET."},
+                          {"type", "invalid_request_error"}
+                      }}
+        });
+    });
+
+    m_httpServer->route("/chat/", QHttpServerRequest::Method::Post, [this, createResponse](const QHttpServerRequest &request) {
+        QJsonParseError parseError;
+        QJsonDocument jsonDoc = QJsonDocument::fromJson(request.body(), &parseError);
+        if (parseError.error != QJsonParseError::NoError || !jsonDoc.isObject()) {
+            return createResponse(QJsonObject{{"error", "Invalid JSON format."}});
+        }
+
+        QJsonObject jsonObj = jsonDoc.object();
+
+        QString model = jsonObj.value("model").toString();
+        QJsonArray messages = jsonObj.value("messages").toArray();
+        double temperature = jsonObj.value("temperature").toDouble(0.7);
+        int maxTokens = jsonObj.value("max_tokens").toInt(-1);
+        bool stream = jsonObj.value("stream").toBool(false);
+
+        QString finalReply = "Today is Thursday, the sky is blue,\nI answer in rhymes, just for you!";
+
+        QJsonObject responseObj{
+            {"model", model},
+            {"temperature", temperature},
+            {"stream", stream},
+            {"max_tokens", maxTokens},
+            {"reply", finalReply}
+        };
+
+        return createResponse(responseObj);
+    });
+
+    m_httpServer->route("/chat/", QHttpServerRequest::Method::Get, [createResponse]() {
+        return createResponse(QJsonObject{
+            {"error", QJsonObject{
+                          {"message", "Only POST requests are accepted on /chat/"},
+                          {"type", "invalid_request_error"},
+                          {"code", "method_not_supported"}
+                      }}
+        });
+    });
+
+    m_httpServer->route("/chat/", [createResponse](const QHttpServerRequest &request) {
+        if (request.method() != QHttpServerRequest::Method::Post &&
+            request.method() != QHttpServerRequest::Method::Get) {
+            return createResponse(QJsonObject{
+                {"error", QJsonObject{
+                              {"message", "HTTP method not allowed."},
+                              {"type", "invalid_request_error"}
+                          }}
+            });
+        }
+        return QHttpServerResponse(QHttpServerResponder::StatusCode::MethodNotAllowed);
+    });
+
+    auto tcpServer = new QTcpServer(this);
+    if (!tcpServer->listen(QHostAddress::Any, m_port) || !m_httpServer->bind(tcpServer)) {
+        qWarning() << "Failed to bind HTTP server to port" << m_port;
+        return;
+    }
+
+    setIsRuning(true);
+    qInfo() << "HTTP server is running on port" << m_port;
 }
 
 ProgramLanguage *CodeDeveloperList::getCurrentProgramLanguage() const{return m_currentProgramLanguage;}
--- core/developer/codedeveloperlist.h
+++ core/developer/codedeveloperlist.h
@@ -4,6 +4,25 @@
 #include <QObject>
 #include <QQmlEngine>
 #include <QAbstractListModel>
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QJsonDocument>
+
+#include <QHostAddress>
+#include <QHttpServer>
+#include <QHttpServerResponder>
+
+#if QT_CONFIG(ssl)
+#  include <QSslCertificate>
+#  include <QSslKey>
+#  include <QSslServer>
+#endif
+
+#include <QFile>
+#include <QJsonObject>
+#include <QString>
+#include <QTcpServer>
+
 #include "./code/programlanguage.h"
 #include "./code/codegenerator.h"
 #include "./code/code_generator/curlcodegenerator.h"
@@ -16,10 +35,12 @@ class CodeDeveloperList: public QAbstractListModel
     Q_OBJECT
     QML_SINGLETON
     Q_PROPERTY(int count READ count NOTIFY countChanged FINAL)
+    Q_PROPERTY(int port READ port NOTIFY portChanged FINAL)
+    Q_PROPERTY(bool isRuning READ isRuning NOTIFY isRuningChanged FINAL)
     Q_PROPERTY(ProgramLanguage *currentProgramLanguage READ getCurrentProgramLanguage WRITE setCurrentProgramLanguage NOTIFY currentProgramLanguageChanged FINAL)
 
 public:
-    static CodeDeveloperList* instance(QObject* parent );
+    static CodeDeveloperList* instance(QObject* parent);
 
     enum CodeDeveloperRoles {
         IDRole = Qt::UserRole + 1,
@@ -27,6 +48,7 @@ public:
     };
 
     Q_INVOKABLE void setCurrentLanguage(int id);
+    Q_INVOKABLE void start();
 
     int count() const;
     int rowCount(const QModelIndex &parent) const override;
@@ -36,15 +58,27 @@ public:
     ProgramLanguage *getCurrentProgramLanguage() const;
     void setCurrentProgramLanguage(ProgramLanguage *newCurrentProgramLanguage);
 
+    int port() const;
+    void setPort(int newPort);
+
+    bool isRuning() const;
+    void setIsRuning(bool newIsRuning);
+
 signals:
     void countChanged();
     void currentProgramLanguageChanged();
+    void portChanged();
+    void isRuningChanged();
 
 private:
     explicit CodeDeveloperList(QObject* parent);
     static CodeDeveloperList* m_instance;
 
     ProgramLanguage *m_currentProgramLanguage;
+
+    int m_port;
+    bool m_isRuning;
+    QHttpServer* m_httpServer = nullptr;
 
     QList<ProgramLanguage*> m_programLanguags;
 };
