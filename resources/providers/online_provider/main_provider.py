35d757aa8cacb70be0514f16de09f77d192abfdf
unknown <ashkan.eskandari.dev@gmail.com>
2025-10-02 20:47:41 +0330

    stream auth

--- resources/providers/online_provider/main_provider.py
+++ resources/providers/online_provider/main_provider.py
@@ -74,10 +74,12 @@ if __name__ == "__main__":
                 if client.stop_generation:
                     sys.stdout.flush()
                     break
+
                 if isinstance(chunk, dict) and "data" in chunk:
                     content = chunk["data"]
                 else:
                     content = chunk
+
                 sys.stdout.write(content)
                 sys.stdout.flush()
                 time.sleep(0.1)
